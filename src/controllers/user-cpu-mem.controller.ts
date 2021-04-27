28087d325858060edf29bce0c243da3c4016121b
Zhengheng Li <zhenghenge@gmail.com>
2021-04-27 16:06:03 +0800

    user-cpu-mem.controller.ts: add schema check for request body

--- src/controllers/user-cpu-mem.controller.ts
+++ src/controllers/user-cpu-mem.controller.ts
@@ -1,14 +1,52 @@
 // Uncomment these imports to begin using these cool features!
 
 // import {inject} from '@loopback/core';
-import { get, post, requestBody } from "@loopback/rest";
+import { get, post, requestBody, SchemaObject } from "@loopback/rest";
 import { repository } from "@loopback/repository";
 import { UserCpuMemRepository } from "../repositories";
+
+const schemaNodeUtil: SchemaObject = {
+    type: "object",
+    properties: {
+        node: { type: "string" },
+        data: {
+            type: "array",
+            items: {
+                type: "object",
+                properties: {
+                    user: { type: "string" },
+                    util: {
+                        type: "object",
+                        properties: {
+                            cpu: { type: "number" },
+                            mem: { type: "number" },
+                        },
+                    },
+                },
+            },
+        },
+    },
+    required: ["node", "data"],
+    additionalProperties: false,
+};
+
+type UserUtil = {
+    user: string;
+    util: {
+        cpu: number;
+        mem: number;
+    };
+};
+
+type NodeUtil = {
+    node: string;
+    data: UserUtil[];
+};
 
 export class UserCpuMemController {
     constructor(@repository(UserCpuMemRepository) private userCpuMemRepo: UserCpuMemRepository) {}
     @post("/user-cpu-mem")
-    async create(@requestBody.array({}) data: Object[]) {
+    async create(@requestBody.array(schemaNodeUtil) data: NodeUtil[]) {
         console.log(data);
         return data;
     }
