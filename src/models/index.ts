207cb607cef47f591f0a0df873d0b92d7212bf99
Zhengheng Li <zhenghenge@gmail.com>
2021-04-28 10:29:08 +0800

    add node-cpu-load.model.ts

--- src/models/index.ts
+++ src/models/index.ts
@@ -1,1 +1,2 @@
 export * from './user-cpu-mem.model';
+export * from './node-cpu-load.model';
---
+++ src/models/node-cpu-load.model.ts
@@ -0,0 +1,106 @@
+import { Entity, model, property } from "@loopback/repository";
+
+@model({
+    settings: {
+        postgresql: { schema: "public", table: "node_cpu_load" },
+    },
+})
+export class NodeCpuLoad extends Entity {
+    @property({
+        type: "number",
+        id: true,
+        generated: true,
+    })
+    id?: number;
+
+    @property({
+        type: "string",
+        required: true,
+        postgresql: {
+            dataType: "varchar(60)",
+        },
+    })
+    node: string;
+
+    @property({
+        type: "number",
+        required: true,
+        postgresql: {
+            dataType: "real",
+        },
+    })
+    user: number;
+
+    @property({
+        type: "number",
+        required: true,
+        postgresql: {
+            dataType: "real",
+        },
+    })
+    system: number;
+
+    @property({
+        type: "number",
+        required: true,
+        postgresql: {
+            dataType: "real",
+        },
+    })
+    idle: number;
+
+    @property({
+        type: "number",
+        required: true,
+        postgresql: {
+            dataType: "real",
+        },
+    })
+    iowait: number;
+
+    @property({
+        type: "number",
+        required: true,
+        postgresql: {
+            dataType: "real",
+        },
+    })
+    irq: number;
+
+    @property({
+        type: "number",
+        required: true,
+        postgresql: {
+            dataType: "real",
+        },
+    })
+    softirq: number;
+
+    @property({
+        type: "number",
+        required: true,
+        postgresql: {
+            dataType: "real",
+        },
+    })
+    steal: number;
+
+    @property({
+        type: "number",
+        required: true,
+        postgresql: {
+            dataType: "real",
+        },
+    })
+    guest: number;
+
+    constructor(data?: Partial<NodeCpuLoad>) {
+        super(data);
+    }
+}
+
+export interface NodeCpuLoadRelations {
+    // describe navigational properties here
+}
+
+export type NodeCpuLoadWithRelations = NodeCpuLoad & NodeCpuLoadRelations;
