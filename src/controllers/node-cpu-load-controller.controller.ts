3cb93dc46522d64e00f102c4d7909c44eac868b8
Zhengheng Li <zhenghenge@gmail.com>
2021-04-28 12:24:35 +0800

    implement post method for cpu load

--- src/controllers/node-cpu-load-controller.controller.ts
+++ src/controllers/node-cpu-load-controller.controller.ts
@@ -4,17 +4,68 @@ import { repository, Filter } from "@loopback/repository";
 import { NodeCpuLoadRepository } from "../repositories";
 import { NodeCpuLoad } from "../models";
 
-const schemaCpuLoad: SchemaObject = {};
+const schemaCpuLoad: SchemaObject = {
+    title: "NodeCpuLoadData",
+    type: "object",
+    properties: {
+        node: { type: "string" },
+        data: {
+            type: "object",
+            properties: {
+                user: { type: "number" },
+                system: { type: "number" },
+                idle: { type: "number" },
+                iowait: { type: "number" },
+                irq: { type: "number" },
+                softirq: { type: "number" },
+                steal: { type: "number" },
+                guest: { type: "number" },
+            },
+            required: ["user", "system", "idle", "iowait", "irq", "softirq", "steal", "guest"],
+            additionalProperties: false,
+        },
+    },
+    required: ["node", "data"],
+    additionalProperties: false,
+};
+
+type NodeCpuLoadData = {
+    node: string;
+    data: {
+        user: number;
+        system: number;
+        idle: number;
+        iowait: number;
+        irq: number;
+        softirq: number;
+        steal: number;
+        guest: number;
+    };
+};
 
 export class NodeCpuLoadControllerController {
     constructor(@repository(NodeCpuLoadRepository) private nodeCpuLoadRepo: NodeCpuLoadRepository) {}
 
     @authenticate("static")
     @post("/node-cpu-load")
-    async create(@requestBody.array(schemaCpuLoad) data: Object[]): Promise<NodeCpuLoad[]> {
-        // const currentTime = new Date().getTime();
-        console.log(data);
-        return [];
+    async create(@requestBody.array(schemaCpuLoad) data: NodeCpuLoadData[]): Promise<NodeCpuLoad[]> {
+        const currentTime = new Date().getTime();
+        const entries: NodeCpuLoad[] = [];
+        for (const x of data) {
+            const entry = new NodeCpuLoad();
+            entry.time = currentTime;
+            entry.node = x.node;
+            entry.user = x.data.user;
+            entry.system = x.data.system;
+            entry.idle = x.data.idle;
+            entry.iowait = x.data.iowait;
+            entry.irq = x.data.irq;
+            entry.softirq = x.data.softirq;
+            entry.steal = x.data.steal;
+            entry.guest = x.data.guest;
+            entries.push(entry);
+        }
+        return this.nodeCpuLoadRepo.createAll(entries);
     }
 
     @authenticate("static")
