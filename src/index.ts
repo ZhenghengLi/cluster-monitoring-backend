9a00a2e430e987d5a9c8870f0424998c0d277a49
Zhengheng Li <zhenghenge@gmail.com>
2021-04-29 15:44:54 +0800

    config openApiSpec

--- src/index.ts
+++ src/index.ts
@@ -20,16 +20,11 @@ if (require.main === module) {
         rest: {
             port: +(process.env.PORT ?? 3000),
             host: process.env.HOST,
-            // The `gracePeriodForClose` provides a graceful close for http/https
-            // servers with keep-alive clients. The default value is `Infinity`
-            // (don't force-close). If you want to immediately destroy all sockets
-            // upon stop, set its value to `0`.
-            // See https://www.npmjs.com/package/stoppable
             gracePeriodForClose: 5000, // 5 seconds
             openApiSpec: {
-                // useful when used with OpenAPI-to-GraphQL to locate your application
-                setServersFromRequest: true,
+                servers: [{ url: process.env.API_SERVER_URL }],
             },
+            basePath: process.env.API_SERVER_BASE_PATH,
         },
     };
     main(config).catch((err) => {
